bb2f2480e0f87ed48c0a7335fb5fffc9fce6ebba
Joseph Smith <joe@layerful.com>
2017-08-03 14:15:46 +0100

    Added script which auto builds routes

--- scripts/route.js
+++ scripts/route.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 module.exports = {
     generate: function(path, component, name = false){
     	const routesFilePath = __dirname + '/../routes/web.js'
+    	const frontendFilePath = __dirname + '/../resources/assets/js/routes.js'
+    	var componentName = component.split('/')
+
+    	//==================================================
     	var routesFile = fs.readFileSync(routesFilePath, 'utf-8').toString().split("\n")
     	routesFile.splice(1, 0, "	{")
     	if(!name) {
@@ -10,37 +14,51 @@ module.exports = {
     	}else{
     		routesFile.splice(2, 0, "		name: '" + name + "',")
     	}
-    	
     	routesFile.splice(3, 0, "		path: '" + path + "',")
-    	routesFile.splice(4, 0, "		component: '" + component + "'")
+    	routesFile.splice(4, 0, "		component: '" + componentName[componentName.length - 1] + 'Page' + "'")
     	routesFile.splice(5, 0, "	},")
     	var text = routesFile.join("\n")
-
     	fs.writeFile(routesFilePath, text, function (err) {
-		  if (err) return console.log(err);
-		});
-
-    	var frontendFilePath = __dirname + '/../resources/assets/js/routes.js'
-    	var frontendFile = fs.readFileSync(frontendFilePath, 'utf-8').toString().split("\n")
-    	var componentName = component.split('/')
-    	frontendFile.splice(1, 0, "import " + componentName[componentName.length - 1] + 'Page' + " from '../../views/" + component + "'")
-    	var text = frontendFile.join("\n")
-
-    	fs.writeFile(frontendFilePath, text, function (err) {
-		  if (err) return console.log(err);
-		});
-
-		for(let i = 0; i < frontendFile.length; i++) {
-			if(frontendFile[i] == "const components = {") {
-				frontendFile.splice(i, 0, componentName[componentName.length - 1] + 'Page')
-				var text = frontendFile.join("\n")
-
-				fs.writeFile(frontendFilePath, text, function (err) {
-				  if (err) return console.log(err);
-				});
+			if (err) {
+				return console.log(err);
+			}else{
+				console.log('Added route to web.js routes file.')
 			}
-		}
-        
+		});
+
+    	//================================================
+    	var frontendFile = fs.readFileSync(frontendFilePath, 'utf-8').toString().split("\n")
+    	frontendFile.splice(1, 0, "import " + componentName[componentName.length - 1] + 'Page' + " from '../../views/" + component + "'")
+
+		let lineNumber = 0
+    	for(let i = 0; i < frontendFile.length; i++) {
+    		if(frontendFile[i] == "const components = {") {
+    			lineNumber = i
+    		}
+    	}
+
+    	if(lineNumber > 0) {
+    		frontendFile.splice(lineNumber + 1, 0, "	" + componentName[componentName.length - 1] + 'Page,')
+    	}
+    	var text = frontendFile.join("\n")
+    	fs.writeFile(frontendFilePath, text, function (err) {
+			if (err) {
+				return console.log(err);
+			}else{
+				console.log('Imported the componented on routes.js in your frontend assets.')
+			}
+		});
+
+		//============================
+		fs.writeFile(__dirname + "/../resources/views/" + component + '.vue', "<template><div>I am your new vue component</div></template>", function(err) {
+    		if(err) {
+        		return console.log(err);
+    		}else{
+    			console.log("A new vue file was saved: " + component + '.vue');
+    		}
+		});
+
+		return "Your new route was successfully added." 
     }
 };
 
