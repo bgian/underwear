4d2750c6eff23b51bc062171028182e1d5368ea9
Joseph Smith <joe@layerful.com>
2017-08-08 03:33:22 +0100

    Added extra database stuff

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 module.exports = {
 	secret: process.env.APP_ID,
 	db: process.env.DB_NAME,
-	url: process.env.DB_HOST + '://' + process.env.DB_SERVER + '/'
+	url: process.env.DB_HOST + '://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_SERVER + ':' + process.env.DB_PORT + '/'
 }
